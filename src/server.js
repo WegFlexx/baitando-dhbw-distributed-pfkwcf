8b10de440962b77013a0540cdb44659f624cead8
WegFlexx <116099986+WegFlexx@users.noreply.github.com>
2023-05-14 15:08:38 +0200

    2 files modified

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const express = require("express");
 const crypto = require("crypto");
-
 const app = express();
+const dataFilePath = './data.json';
 
 app.use(express.json());
-
-const dataFilePath = './data.json';
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('yaml').parse(fs.readFileSync('./spec/powertrack.yaml', 'utf8'));
@@ -30,98 +28,124 @@ function validateRecord(record) {
         record.reading && typeof record.reading === "number";
 }
 
-// Helper function to read the data from the JSON file
+// Function to write the data to the JSON file
+function writeDataToFile(data) {
+    try {
+        fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
+        return true;
+    } catch (writingError) {
+        console.error('Error writing data to file:', writingError);
+        return false;
+    }
+}
+
+// Function to read the data from the JSON file
 function readDataFromFile() {
     try {
       const data = fs.readFileSync(dataFilePath, 'utf8');
       return JSON.parse(data);
-    } catch (err) {
-      console.error('Error reading data from file:', err);
+    } catch (readingError) {
+      console.error('Error reading data from file:', readingError);
       return [];
     }
-  }
-  
-  // Helper function to write the data to the JSON file
-  function writeDataToFile(data) {
-    try {
-      fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
-    } catch (err) {
-      console.error('Error writing data to file:', err);
-    }
-  }
+}
 
-  // GET /records
-app.get('/records', (req, res) => {
+// GET /records
+app.get('/records', (req, response) => {
     const data = readDataFromFile();
-    res.json({ items: data });
-  });
+    if (data) {
+        response.json({ items: data });
+        response.status(200).send('List of power records retrieved successfully.')
+    }
+    else {
+        response.status(500).send('Internal server error. The response payload is empty.')
+    }
+});
   
-  // POST /records
-  app.post('/records', (req, res) => {
-    const { date, reading } = req.body;
-  
+// POST /records
+app.post('/records', (request, response) => {
+    const { date, reading } = request.body;
+
     if (!date || !reading) {
-      res.status(400).send('Invalid request data');
-      return;
+        response.status(400).send('Invalid request data. Occurs, if a required attribute in the request payload is missing. The response payload is empty.');
+        return;
     }
-  
-    const isValid = validateRecord(req.body);
+
+    const isValid = validateRecord(request.body);
     if (!isValid) {
-      res.status(400).send('Invalid record data');
-      return;
+        response.status(400).send('Invalid request data. Occurs, if a required attribute in the request payload is missing. The response payload is empty.');
+        return;
     }
-  
+
     const data = readDataFromFile();
     const id = generateId();
     const record = { id, date, reading };
     data.push(record);
-    writeDataToFile(data);
-  
-    const recordUrl = getRecordUrl(req, id);
-    res.status(201).header('Location', recordUrl).send();
-  });
-  
-  // GET /records/{record-id}
-  app.get('/records/:recordId', (req, res) => {
-    const { recordId } = req.params;
-  
-    const data = readDataFromFile();
-    const record = data.find((item) => item.id === recordId);
-  
-    if (!record) {
-      res.status(404).send('Record not found');
-      return;
+    let writingSuccessfull = writeDataToFile(data);
+
+    if (writingSuccessfull) {
+        const recordUrl = getRecordUrl(request, id);
+        response.status(201).header('Location', recordUrl).send('Record created successfully. The response payload is empty.');
     }
+    else {
+        response.status(500).send('Internal server error. The response payload is empty.');
+    }
+});
   
-    res.json(record);
-  });
-  
-  // DELETE /records/{record-id}
-  app.delete('/records/:recordId', (req, res) => {
+// GET /records/{record-id}
+app.get('/records/:recordId', (req, res) => {
     const { recordId } = req.params;
+    const data = readDataFromFile();
+    if (data === []) {
+        res.status(500).send('Internal server error. The response payload is empty.');
+        return;  
+    }
+    const record = data.find((item) => item.id === recordId);
+
+    if (!record) {
+        res.status(404).send('Power record with given ID does not exist. The response payload is empty.');
+        return;
+    }
+    else {
+        res.json(record);
+        res.status(200).send('Power record retrieved successfully.');
+    }
+});
   
+// DELETE /records/{record-id}
+app.delete('/records/:recordId', (req, res) => {
+    const { recordId } = req.params;
     const data = readDataFromFile();
     const index = data.findIndex((item) => item.id === recordId);
-  
+
     if (index === -1) {
-      res.status(404).send('Record not found');
-      return;
+        res.status(404).send('Power record with given ID does not exist. The response payload is empty.');
+        return;
     }
-  
+
     data.splice(index, 1);
-    writeDataToFile(data);
+    let writingSuccessfull = writeDataToFile(data);
+    if (writingSuccessfull) {
+        res.status(204).send('Power record deleted successfully. The response payload is empty.');
+    }
+    else {
+        res.status(500).send('Internal server error. The response payload is empty.');
+    }
+});
   
-    res.status(204).send();
-  });
-  
-  // DELETE /records
-  app.delete('/records', (req, res) => {
-    writeDataToFile([]);
-    res.status(204).send();
-  });
-  
-  // Error handler
-  app.use((err, req, res, next) => {
+// DELETE /records
+app.delete('/records', (req, res) => {
+    let writingSuccessfull = writeDataToFile([]);
+    if (writingSuccessfull) {
+        res.status(204).send('All power records deleted successfully. The response payload is empty.');
+    }
+    else {
+        res.status(500).send('Internal server error. The response payload is empty.');
+    }
+});
+
+// Error handler
+app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
-  });
+});
